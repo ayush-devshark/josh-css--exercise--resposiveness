70440cc46de872a55c91252686334482e3bf9188
ayush-devshark <ayushagarwal.dev@gmail.com>
2022-07-19 15:55:44 +0530

    add media query

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
+import { QUERIES } from '../../constants';
+
 import Header from '../Header';
 import ShoeIndex from '../ShoeIndex';
 
 const App = () => {
-  const [sortId, setSortId] = React.useState('newest');
+    const [sortId, setSortId] = React.useState('newest');
 
-  return (
-    <>
-      <Header />
-      <Main>
-        <ShoeIndex sortId={sortId} setSortId={setSortId} />
-      </Main>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Main>
+                <ShoeIndex sortId={sortId} setSortId={setSortId} />
+            </Main>
+        </>
+    );
 };
 
 const Main = styled.main`
-  padding: 64px 32px;
+    padding: 64px 32px;
+
+    /* @media ${QUERIES.laptopAndSmaller}{
+        background:yellow;
+        
+    }
+    @media ${QUERIES.tabletAndSmaller}{
+        background:red;
+
+    }
+    @media ${QUERIES.phoneAndSmaller}{
+        background:pink;
+
+    } */
 `;
 
 export default App;
